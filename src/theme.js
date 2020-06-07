ea8aa895e80cdd7b7457470b2d1fcdb5d3ddbe0e
brookecheney <brookecheney@hotmail.com>
2020-06-07 13:36:57 -0700

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#00e676',
+          },
+        secondary: {
+            main: '#f44336',
+          },
+    }
+})
+
+
+export default theme;
