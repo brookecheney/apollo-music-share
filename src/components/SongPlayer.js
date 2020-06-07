82a93f278901ecbd659b9654143ce4a16f034959
brookecheney <brookecheney@hotmail.com>
2020-06-07 16:56:46 -0700

    Update SongPlayer.js

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import QueuedSongList from './QueuedSongList';
+import { Card, CardContent, Typography } from "@material-ui/core";
 
 function SongPlayer() {
-    return <div>songplayer
+    return (
 
-        <QueuedSongList />
-    </div>
+        <>
+        <Card variant="outlined">
+            <div>
+                <CardContent>
+                    <Typography variant="h5" component="h3">
+                🇹🇱 Title
+
+
+
+                    </Typography>
+                    <Typography variant="subtitle1" component="p" color="textSecondary">
+                        Artist
+                    </Typography>
+                </CardContent>
+             
+            </div>
+        </Card>
+
+
+</>
+    )
+
 }
 
 
