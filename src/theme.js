64861f077fb6a78476d0048e4a03390ea3211822
brookecheney <brookecheney@hotmail.com>
2020-06-07 16:56:45 -0700

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,12 +3,12 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
     palette: {
-        type: 'dark',
+        type: 'light',
         primary: {
             main: '#00e676',
           },
         secondary: {
-            main: '#f44336',
+            main: '#ff1dce',
           },
     }
 })
