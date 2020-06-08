e43cb76ca29c67b123bd17b9d907c0cf3f4ed3f3
brookecheney <brookecheney@hotmail.com>
2020-06-08 13:28:02 -0700

    Update QueuedSongList.js

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,11 +1,75 @@
 import React from 'react'
+import { Typography, Avatar, IconButton, makeStyles } from "@material-ui/core";
+import { Delete } from '@material-ui/icons'
 
 function QueuedSongList() {
+    const song = {
+        title: "Stolen Dance",
+        artist: "Le P",
+        thumbnail: "https://images.unsplash.com/photo-1591211880384-91e671e14f99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
+    };
+
+
     return (
     <div style={{ margin: '10px 0' }}>
+        <Typography color="textSecondary" variant="button">
+            QUEUE (5)
+
+        </Typography>
+        {Array.from({ length: 5 }, () => song).map((song, i) => (
+        <QueuedSong key={i} song={song} />
+        ))}
 
     </div>
     )
+
+}
+
+
+const useStyles = makeStyles({
+    avatar: {
+        width: 44,
+        height: 44
+    },
+    text: {
+        textOverflow: 'hidden'
+    },
+    container: {
+        display: 'grid',
+        gridAutoFlow: 'column',
+        gridTemplateColumns: '50px auto 50px',
+        gridGap: 12,
+        alignItems: 'center',
+        marginTop: 10
+    },
+    songInfoContainer: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+
+    }
+})
+
+
+
+
+function QueuedSong({ song }) {
+    const classes = useStyles()
+    const { thumbnail, artist, title } = song 
+    return <div className={classes.container}>
+        <Avatar src={thumbnail} alt="song thumnail" className={classes.avatar} />
+        <div className={classes.songInfoContainer} >
+            <Typography variant= "subtitle2" className={classes.text}>
+                {title}
+            </Typography>
+            <Typography  color="textSecondary" variant= "body2">
+                {title}
+            </Typography>
+
+        </div>
+        <IconButton>
+           <Delete color="error" />  
+        </IconButton>
+    </div>;
 }
 
 
