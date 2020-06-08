0c181745237f3920e8ef3ffd3b69f76dd1064cb8
brookecheney <brookecheney@hotmail.com>
2020-06-08 13:28:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import AddSong from './components/AddSong';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
-import { Grid } from '@material-ui/core'
+import { Grid, useMediaQuery, greaterThanMd } from '@material-ui/core'
 
 
 
 function App() {
+  const matches = useMediaQuery(theme => theme.breakpoints.down('md'));
+  console.log(matches);
   return (
    <>
    
@@ -20,13 +22,19 @@ function App() {
 <SongList />
 </Grid>
 <Grid 
-style = {{
-  position: 'fixed',
-  width: '100%',
+style = {
+  greaterThanMd ? {
+  position: "fixed",
+  width: "100%",
   right: '0',
   top: 70,
 
-}} item xs={12} md={5}>
+} : {
+  position: 'fixed',
+  left: 0,
+  bottom: 0
+}} 
+item xs={12} md={5}>
 <SongPlayer />
 </Grid>
 </Grid>
