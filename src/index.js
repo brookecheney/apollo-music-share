a192d025e951669c16d24ee09613c6f52e0cf0c2
brookecheney <brookecheney@hotmail.com>
2020-06-08 17:38:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import { ApolloProvider } from '@apollo/react-hooks'
 import theme from "./theme";
+import client from "./graphql/client";
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </MuiThemeProvider>,
+  </MuiThemeProvider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
