16ecff7b900527972f71a7f31b21ed77dee45b70
brookecheney <brookecheney@hotmail.com>
2020-06-12 12:45:32 -0700

    Update queries.js

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,18 +1,20 @@
-import { gql } from 'apollo-boost'
+import { gql } from 'apollo-boost';
 
 
 
-export const GET_SONGS = gql`
+ export const GET_QUEUED_SONGS  = gql`
 
 
-query getSongs {
-  songs(order_by: {created_at: desc}) {
-    artist
-    duration
-    id
-    thumbnail
-    title
-    url
-  }
+query getQueuedSongs {
+    queue @client {
+        id
+        duration
+        title
+        artist
+        thumbnail
+        url
+
+    }
 }
+
 `
