08439bc66cd043311f03c857683c3a9a1c089977
brookecheney <brookecheney@hotmail.com>
2020-06-12 12:45:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import Header from './components/Header';
 import AddSong from './components/AddSong';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
-import { Grid, useMediaQuery, Hidden } from '@material-ui/core'
+import { Grid, useMediaQuery, Hidden } from '@material-ui/core';
+import songReducer from "./reducer"
+
+export const SongContext = React.createContext({
+  song: {
+    id: "2989d343-b39a-464a-9555-24a0afa81133",
+    title: "AMAZING! Arthur Gunn Performs “Kiss The Girl” For Disney Night - American Idol 2020",
+    artist:"Arthur Gunn",
+    thumbnail: "http://img.youtube.com/vi/rZXEVkJtm1A/0.jpg",
+    url: "https://youtu.be/rZXEVkJtm1A",
+    duration: "229"
+  },
+  isPlaying: false
+});
 
 
 
 function App() {
+  const initialSongState = React.useContext(SongContext)
+  const [state, dispatch] = React.useReducer(songReducer, initialSongState)
   const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
   const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'));
  
   return (
-   <>
+   <SongContext.Provider value= {{ state, dispatch }}>
    <Hidden only="xs">
 <Header />
    </Hidden>
@@ -42,7 +57,7 @@ item xs={12} md={5}>
 </Grid>
 </Grid>
 
-   </>
+   </SongContext.Provider>
   )
 }
 
